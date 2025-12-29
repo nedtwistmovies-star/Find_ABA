6e029bfbc7cf79b159da7d90c3bb64d385d869e6
SANDALSroyalle <nedtwistmovies@gmail.com>
2025-12-29 12:28:49 +0100

    feat: configure supabase client
    Ssupabase client initialization

---
+++ src/services/database/supabase. services.ts
@@ -0,0 +1,8 @@
+Ts
+import { createClient } from "@supabase/supabase-js";
+import { env } from "../../config/env";
+
+export const supabase = createClient(
+  env.supabaseUrl,
+  env.supabaseKey
+);
