925fe7a2c1759d50ee56ef4b9b5a89a34234643d
SANDALSroyalle <nedtwistmovies@gmail.com>
2025-12-31 21:00:53 +0100

    Create index.tsx

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>FindAba is Live 🚀</h1>
+      <p>Next.js is running successfully.</p>
+    </main>
+  );
+}
