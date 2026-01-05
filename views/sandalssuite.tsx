234a0ec1fb094c2e04a31cf2aa2f0a5504a09224
SANDALSroyalle <nedtwistmovies@gmail.com>
2026-01-05 07:44:02 +0100

    feat: add SandalsSuite ecosystem showcase view
    Showcase SANDALSroyalle ecosystem modules.

---
+++ views/sandalssuite.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SandalsSuite: React.FC = () => {
+  return (
+    <div>
+      <h2>SANDALSroyalle Suite</h2>
+      <ul>
+        <li>Events & Protocol Services</li>
+        <li>Cleaners & Movers</li>
+        <li>City Logistics & Discovery</li>
+        <li>Creative Economy Hub</li>
+      </ul>
+    </div>
+  );
+};
+
+export default SandalsSuite;
